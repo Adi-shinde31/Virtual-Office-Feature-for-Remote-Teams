2efe7b267f7c7dd47ad73246dfe8e266e013fe4f
Aditya <shindeaditya0258@gmail.com>
2025-03-28 00:30:17 -0500

    Successfully moving character on WASD command - Task 1

--- client/src/components/characterConstants.js
+++ client/src/components/characterConstants.js
@@ -5,16 +5,16 @@ export const CHARACTER_IMAGE_SIZE = 32;
 
 export const CHARACTER_CLASSES_MAP = {
     ENGINEER: {
-        icon: {sx: 0, sy: 0},
-        portrait: {sx: 0, sy: 240},
+        icon: { sx: 0, sy: 0 },
+        portrait: { sx: 0, sy: 240 },
         className: 'ENGINEER',
-        spriteImage: 'assets/characters/characters.png',        
+        spriteImage: 'assets/characters/characters.png',
     },
 };
 
 export const MY_CHARACTER_INIT_CONFIG = {
     name: 'Amanda',
     id: uuid(),
-    position: {x: 12, y: 12},
+    position: { x: 12, y: 12 },
     characterClass: 'ENGINEER',
 };
